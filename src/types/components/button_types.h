4cfe78328fe39b0ae057fa0b62092a249412527a
Joshua Matthews <jmatth93@gmail.com>
2025-03-18 12:31:51 -0500

    add doc strings:

--- src/types/components/button_types.h
+++ src/types/components/button_types.h
@@ -15,10 +15,16 @@ struct button_t {
   void *context;
   uint32_t id;
   struct theme_t theme;
+  /**
+   * Properties h and w can be set to -1 to allow automatic sizing based on the label dimensions.
+   */
   SDL_FRect rect;
   SDL_Texture* texture;
   button_callback onClick;
   struct label_t label;
+  /**
+   * Flag to use the x and y properties of rect as center position instead of top left corner.
+   */
   bool center;
 };
 
